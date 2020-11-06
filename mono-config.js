56d2d60f956b271b47ee4f01ef8a13ec46956262
Aleksey Kliger <alklig@microsoft.com>
2020-11-06 16:52:35 -0500

    initial commit

---
+++ mono-config.js
@@ -0,0 +1,92 @@
+config = {
+  "assembly_root": "managed",
+  "debug_level": 0,
+  "assets": [
+    {
+      "behavior": "assembly",
+      "name": "EncHelper.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Collections.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Console.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.IO.FileSystem.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Linq.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Memory.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Private.CoreLib.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Private.Runtime.InteropServices.JavaScript.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Private.Uri.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Runtime.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Runtime.CompilerServices.Unsafe.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Runtime.InteropServices.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Text.Encoding.Extensions.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "System.Threading.dll"
+    },
+    {
+      "behavior": "assembly",
+      "name": "WasmSample.dll"
+    },
+    {
+      "virtual_path": "/WasmSample.dll.1.dmeta",
+      "behavior": "vfs",
+      "name": "supportFiles/0_WasmSample.dll.1.dmeta"
+    },
+    {
+      "virtual_path": "/WasmSample.dll.1.dil",
+      "behavior": "vfs",
+      "name": "supportFiles/1_WasmSample.dll.1.dil"
+    },
+    {
+      "virtual_path": "/WasmSample.dll.1.dpdb",
+      "behavior": "vfs",
+      "name": "supportFiles/2_WasmSample.dll.1.dpdb"
+    },
+    {
+      "load_remote": false,
+      "behavior": "icu",
+      "name": "icudt.dat"
+    },
+    {
+      "virtual_path": "/usr/share/zoneinfo/",
+      "behavior": "vfs",
+      "name": "dotnet.timezones.blat"
+    }
+  ],
+  "remote_sources": []
+};
